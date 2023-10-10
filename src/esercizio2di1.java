2b0f5dc589ca8e7f12e13b92978ec7fc9ed9dad1
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-10 23:18:48 +0200

    First Commit

---
+++ src/esercizio2di1.java
@@ -0,0 +1,10 @@
+public class esercizio2di1 {
+    public static void main(String[] args) {
+        double a = 231.4;
+        double b = 324.6;
+        
+        double somma = a + b;
+        int numeroInt = (int)somma;
+        System.out.println("numeroInt = " + numeroInt);
+    }
+}
